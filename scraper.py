f96df2d0d9519edab6c96cea4e330483b3325c6d
Jesse Klein <jesse@Jesses-MacBook-Air.local>
2015-07-07 15:08:40 -0700

    final commit for master

--- scraper.py
+++ scraper.py
@@ -54,6 +54,70 @@ def extract_data_listings(parsed):
     return parsed.find_all('div', id=id_finder)
 
 
+def has_two_tds(element):
+    if element.name == 'tr':
+        td_children = element.find_all('td', recursive=False)
+        if len(td_children) == 2:
+            return True
+    return False
+
+
+def clean_data(cell):
+    try:
+        cleaned_up = cell.string.strip(" \n:-")
+        return cleaned_up
+    except AttributeError:
+        return u""
+
+
+def extract_restaurant_metadata(element):
+    metadata_rows = element.find('tbody').find_all(
+        has_two_tds, recursive=False
+    )
+    rdata = {}
+    current_label = ''
+    for row in metadata_rows:
+        key_cell, val_cell = row.find_all('td', recursive=False)
+        new_label = clean_data(key_cell)
+        current_label = new_label if new_label else current_label
+        rdata.setdefault(current_label, []).append(clean_data(val_cell))
+    return rdata
+
+
+def is_inspection_row(element):
+    if element.name == 'tr':
+        td_children = element.find_all('td', recursive=False)
+        has_four = len(td_children) == 4
+        this_text = clean_data(td_children[0]).lower()
+        contains_word = 'inspection' in this_text
+        does_not_start = not this_text.startswith('inspection')
+        return has_four and contains_word and does_not_start
+    return False
+
+
+def extract_score_data(element):
+    inspection_rows = element.find_all(is_inspection_row)
+    samples = len(inspection_rows)
+    total = high_score = average = 0
+    for row in inspection_rows:
+        strval = clean_data(row.find_all('td')[2])
+        try:
+            intval = int(strval)
+        except (ValueError, TypeError):
+            samples -= 1
+        else:
+            total += intval
+            high_score = intval if intval > high_score else high_score
+    if samples:
+        average = total / float(samples)
+    data = {
+        u'Average Score': average,
+        u'High Score': high_score,
+        u'Total Inspections': samples
+    }
+    return data
+
+
 if __name__ == "__main__":
     kwargs = {
         'Inspection_Start': '2/1/2013',
@@ -66,7 +130,8 @@ if __name__ == "__main__":
         html, encoding = get_inspection_page(**kwargs)
 
     doc = parse_source(html, encoding)
-    doc.prettify(encoding=encoding)
     listings = extract_data_listings(doc)
-    print len(listings)
-    print listings[0].prettify()
+    for listing in listings[:5]:
+        metadata = extract_restaurant_metadata(listing)
+        score_data = extract_score_data(listing)
+        print score_data
